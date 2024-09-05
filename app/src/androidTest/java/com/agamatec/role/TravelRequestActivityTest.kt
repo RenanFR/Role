a24a67ff7790743239824cce27dc6c9da2d7fa57
renan.rodrigues.glo <renan.rodrigues.3@globalhitss.com.br>
2024-09-05 18:46:32 -0300

    In progress with form with segmented dates

--- app/src/androidTest/java/com/agamatec/role/TravelRequestActivityTest.kt
+++ app/src/androidTest/java/com/agamatec/role/TravelRequestActivityTest.kt
@@ -2,13 +2,19 @@ package com.agamatec.role
 
 import android.view.View
 import android.widget.DatePicker
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
@@ -21,7 +27,7 @@ class TravelRequestActivityTest {
     val activityRule = ActivityScenarioRule(TravelRequestActivity::class.java)
 
     @Test
-    fun preencherFormularioEApertarEnviar() {
+    fun preencherFormularioComSegmentacaoDeDatas() {
 
 
         onView(withId(R.id.name_edit_text)).perform(
@@ -40,67 +46,123 @@ class TravelRequestActivityTest {
         onView(withId(R.id.birthdate_date_picker)).perform(setDate(1997, 6, 18))
 
 
-
         onView(withId(R.id.gender_spinner)).perform(click())
         onView(withText("Masculino")).perform(click())
 
 
-        onView(withId(R.id.add_destination_button)).perform(click())
+        onView(withId(R.id.add_destination_button)).perform(scrollTo(), click())
 
 
-        onView(withId(R.id.destination_city_edit_text)).perform(
-            scrollTo(), typeText("Madrid"), closeSoftKeyboard()
+        onView(withId(R.id.segment_dates_checkbox)).perform(scrollTo(), click())
+
+
+        onView(withId(R.id.departure_date_picker)).perform(
+            scrollTo(), setDate(2024, 10, 26)
         )
-        onView(withId(R.id.destination_country_edit_text)).perform(
-            scrollTo(), typeText("Espanha"), closeSoftKeyboard()
-        )
-
-
-        onView(withId(R.id.budget_edit_text)).perform(
-            scrollTo(), typeText("1000 EUR"), closeSoftKeyboard()
-        )
-
-
-        onView(withId(R.id.hotel_address_edit_text)).perform(
-            scrollTo(), replaceText(
-                "Paseo Reina Cristina, 7, Retiro, 28014\n" + "Madrid, España"
-            ), closeSoftKeyboard()
+        onView(withId(R.id.return_date_picker)).perform(
+            scrollTo(), setDate(2024, 10, 28)
         )
 
 
         onView(withId(R.id.hotel_reserved_checkbox)).perform(scrollTo(), click())
+        onView(withId(R.id.hotel_address_edit_text)).perform(
+            scrollTo(),
+            replaceText("Paseo Reina Cristina, 7, Retiro, 28014 Madrid, España"),
+            closeSoftKeyboard()
+        )
 
 
-        onView(withId(R.id.departure_date_picker)).perform(scrollTo(), setDate(2024, 9, 15))
-        onView(withId(R.id.return_date_picker)).perform(scrollTo(), setDate(2024, 9, 20))
+        onView(withId(R.id.destination_country_edit_text)).perform(
+            scrollTo(), typeText("Espanha"), closeSoftKeyboard()
+        )
+        onView(withId(R.id.destination_city_edit_text)).perform(
+            scrollTo(), typeText("Madrid"), closeSoftKeyboard()
+        )
+        onView(withId(R.id.budget_edit_text)).perform(
+            scrollTo(), typeText("517.50 EUR"), closeSoftKeyboard()
+        )
 
 
-        onView(withId(R.id.travel_purpose_edit_text)).perform(
-            scrollTo(), replaceText("Férias"), closeSoftKeyboard()
+        onView(withId(R.id.add_date_button)).perform(scrollTo(), click())
+        onView(withId(R.id.departure_date_picker)).perform(
+            scrollTo(), setDate(2024, 11, 3)
+        )
+        onView(withId(R.id.return_date_picker)).perform(
+            scrollTo(), setDate(2024, 11, 7)
+        )
+
+
+        onView(withId(R.id.add_destination_button)).perform(scrollTo(), click())
+
+
+        onView(withId(R.id.hotel_reserved_checkbox)).perform(scrollTo(), click())
+        onView(withId(R.id.hotel_address_edit_text)).perform(
+            scrollTo(),
+            replaceText("Lepanto, 406, Horta-Guinardó, 08025 Barcelona, Espanha"),
+            closeSoftKeyboard()
+        )
+        onView(withId(R.id.destination_country_edit_text)).perform(
+            scrollTo(), typeText("Espanha"), closeSoftKeyboard()
+        )
+        onView(withId(R.id.destination_city_edit_text)).perform(
+            scrollTo(), typeText("Barcelona"), closeSoftKeyboard()
+        )
+        onView(withId(R.id.budget_edit_text)).perform(
+            scrollTo(), typeText("517.50 EUR"), closeSoftKeyboard()
+        )
+
+
+        onView(withId(R.id.departure_date_picker)).perform(
+            scrollTo(), setDate(2024, 10, 28)
+        )
+        onView(withId(R.id.return_date_picker)).perform(
+            scrollTo(), setDate(2024, 11, 1)
+        )
+
+
+        onView(withId(R.id.add_destination_button)).perform(scrollTo(), click())
+
+
+        onView(withId(R.id.hotel_reserved_checkbox)).perform(scrollTo(), click())
+        onView(withId(R.id.hotel_address_edit_text)).perform(
+            scrollTo(),
+            replaceText("Aragón, 28, 07800 Cidade de Ibiza, Espanha"),
+            closeSoftKeyboard()
+        )
+        onView(withId(R.id.destination_country_edit_text)).perform(
+            scrollTo(), typeText("Espanha"), closeSoftKeyboard()
+        )
+        onView(withId(R.id.destination_city_edit_text)).perform(
+            scrollTo(), typeText("Ibiza"), closeSoftKeyboard()
+        )
+        onView(withId(R.id.budget_edit_text)).perform(
+            scrollTo(), typeText("517.50 EUR"), closeSoftKeyboard()
+        )
+
+
+        onView(withId(R.id.departure_date_picker)).perform(
+            scrollTo(), setDate(2024, 11, 7)
+        )
+        onView(withId(R.id.return_date_picker)).perform(
+            scrollTo(), setDate(2024, 11, 9)
         )
 
 
         onView(withId(R.id.add_traveler_button)).perform(scrollTo(), click())
-
-
         onView(withId(R.id.additional_traveler_name_edit_text)).perform(
             scrollTo(), replaceText("Maria Jéssica Pereira Mendes"), closeSoftKeyboard()
         )
         onView(withId(R.id.additional_traveler_filiation_edit_text)).perform(
             scrollTo(), replaceText("Noiva"), closeSoftKeyboard()
         )
-
-
-
         onView(withId(R.id.additional_traveler_birth_date_picker)).perform(
             scrollTo(), setDate(1997, 8, 7)
         )
-
-
-
-
         onView(withId(R.id.additional_traveler_gender_spinner)).perform(scrollTo(), click())
         onView(withText("Feminino")).perform(click())
+
+
+        onView(withId(R.id.confirm_traveler_button)).perform(scrollTo(), click())
 
 
         onView(withId(R.id.notes_edit_text)).perform(
@@ -112,7 +174,6 @@ class TravelRequestActivityTest {
 
         onView(withId(R.id.submit_button)).perform(scrollTo(), click())
     }
-
 
     private fun setDate(year: Int, month: Int, day: Int): ViewAction {
         return object : ViewAction {
--- app/src/main/java/com/agamatec/role/TravelRequestActivity.kt
+++ app/src/main/java/com/agamatec/role/TravelRequestActivity.kt
@@ -3,24 +3,31 @@ package com.agamatec.role
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.*
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
-import java.util.*
 
 class TravelRequestActivity : AppCompatActivity() {
 
     private lateinit var travelersLayout: LinearLayout
     private lateinit var destinationsLayout: LinearLayout
     private var travelerCount = 1
-    private var destinationCount = 1
+    private val travelersList = mutableListOf<JSONObject>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,28 +56,119 @@ class TravelRequestActivity : AppCompatActivity() {
 
     private fun addTravelerForm() {
         Log.d("TravelRequestActivity", "addTravelerForm: Adicionando formulário de viajante")
+
+
+        if (travelersLayout.childCount > 0) {
+            travelersLayout.removeAllViews()
+        }
+
         val travelerForm = layoutInflater.inflate(R.layout.traveler_form, null)
         travelersLayout.addView(travelerForm)
-        travelerCount++
+
+
+        val addTravelerButton = travelerForm.findViewById<Button>(R.id.confirm_traveler_button)
+
+        addTravelerButton.setOnClickListener {
+
+            val travelerName =
+                travelerForm.findViewById<EditText>(R.id.additional_traveler_name_edit_text).text.toString()
+            val travelerBirthDatePicker =
+                travelerForm.findViewById<DatePicker>(R.id.additional_traveler_birth_date_picker)
+            val travelerGender =
+                travelerForm.findViewById<Spinner>(R.id.additional_traveler_gender_spinner).selectedItem.toString()
+
+            val day = travelerBirthDatePicker.dayOfMonth
+            val month = travelerBirthDatePicker.month + 1
+            val year = travelerBirthDatePicker.year
+            val travelerBirthDate = "$day/$month/$year"
+
+
+            val travelerJson = JSONObject().apply {
+                put("name", travelerName)
+                put("birthdate", travelerBirthDate)
+                put("gender", travelerGender)
+            }
+
+
+            travelersList.add(travelerJson)
+
+
+            val travelerLabel = TextView(this)
+            travelerLabel.text =
+                "Viajante: $travelerName - Nascimento: $travelerBirthDate - Gênero: $travelerGender"
+            travelersLayout.addView(travelerLabel)
+
+
+            travelersLayout.removeView(travelerForm)
+        }
     }
 
     private fun addDestinationForm() {
-        Log.d("TravelRequestActivity", "addDestinationForm: Adicionando formulário de destino")
+
+        if (destinationsLayout.childCount > 0) {
+            destinationsLayout.removeAllViews()
+        }
+
         val destinationForm = layoutInflater.inflate(R.layout.destination_form, null)
         destinationsLayout.addView(destinationForm)
-        destinationCount++
+
+        val datesLayout = destinationForm.findViewById<LinearLayout>(R.id.dates_layout)
+        val addDateButton = destinationForm.findViewById<Button>(R.id.add_date_button)
+        val segmentDatesCheckbox =
+            destinationForm.findViewById<CheckBox>(R.id.segment_dates_checkbox)
+
+        val initialDatePair = layoutInflater.inflate(R.layout.date_pair_form, null)
+        datesLayout.addView(initialDatePair)
+
+        val datesList = mutableListOf<Pair<String, String>>()
+
+        segmentDatesCheckbox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                addDateButton.visibility = View.VISIBLE
+            } else {
+                addDateButton.visibility = View.GONE
+
+                while (datesLayout.childCount > 1) {
+                    datesLayout.removeViewAt(1)
+                }
+                datesList.clear()
+            }
+        }
+
+        addDateButton.visibility = View.GONE
+
+        addDateButton.setOnClickListener {
+            val datePairView = layoutInflater.inflate(R.layout.date_pair_form, null)
+
+            if (datesLayout.childCount > 0) {
+                datesLayout.removeAllViews()
+            }
+
+            datesLayout.addView(datePairView)
+
+            val departureDate = datePairView.findViewById<DatePicker>(R.id.departure_date_picker)
+            val returnDate = datePairView.findViewById<DatePicker>(R.id.return_date_picker)
+
+            val departureDateFormatted =
+                "${departureDate.dayOfMonth}/${departureDate.month + 1}/${departureDate.year}"
+            val returnDateFormatted =
+                "${returnDate.dayOfMonth}/${returnDate.month + 1}/${returnDate.year}"
+
+            datesList.add(Pair(departureDateFormatted, returnDateFormatted))
+
+            val dateLabel = TextView(this)
+            dateLabel.text = "Ida: $departureDateFormatted - Volta: $returnDateFormatted"
+            datesLayout.addView(dateLabel)
+        }
     }
 
     private fun submitForm() {
         Log.d("TravelRequestActivity", "submitForm: Iniciando processo de envio de formulário")
 
         try {
-
-
             val name = findViewById<EditText>(R.id.name_edit_text).text.toString()
             val cpf = findViewById<EditText>(R.id.cpf_edit_text).text.toString()
             val email = findViewById<EditText>(R.id.email_edit_text).text.toString()
-
 
             val birthdatePicker = findViewById<DatePicker>(R.id.birthdate_date_picker)
             val birthdate =
@@ -78,73 +176,54 @@ class TravelRequestActivity : AppCompatActivity() {
 
             val gender = findViewById<Spinner>(R.id.gender_spinner).selectedItem.toString()
 
-
             val destinations = JSONArray()
             for (i in 0 until destinationsLayout.childCount) {
                 val destinationView = destinationsLayout.getChildAt(i)
+                val datesLayout = destinationView.findViewById<LinearLayout>(R.id.dates_layout)
 
-                val departureDate =
-                    (destinationView.findViewById<DatePicker>(R.id.departure_date_picker)).let {
-                        "${it.dayOfMonth}/${it.month + 1}/${it.year}"
+                val datesArray = JSONArray()
+                for (j in 0 until datesLayout.childCount) {
+                    val datePairView = datesLayout.getChildAt(j)
+
+                    val departureDate =
+                        (datePairView.findViewById<DatePicker>(R.id.departure_date_picker)).let {
+                            "${it.dayOfMonth}/${it.month + 1}/${it.year}"
+                        }
+                    val returnDate =
+                        (datePairView.findViewById<DatePicker>(R.id.return_date_picker)).let {
+                            "${it.dayOfMonth}/${it.month + 1}/${it.year}"
+                        }
+                    val hotelReserved =
+                        destinationView.findViewById<CheckBox>(R.id.hotel_reserved_checkbox).isChecked
+                    val hotelAddress =
+                        destinationView.findViewById<EditText>(R.id.hotel_address_edit_text).text.toString()
+                    val country =
+                        destinationView.findViewById<EditText>(R.id.destination_country_edit_text).text.toString()
+                    val city =
+                        destinationView.findViewById<EditText>(R.id.destination_city_edit_text).text.toString()
+                    val budget =
+                        destinationView.findViewById<EditText>(R.id.budget_edit_text).text.toString()
+                    val purpose =
+                        destinationView.findViewById<EditText>(R.id.travel_purpose_edit_text).text.toString()
+
+                    val datePairJson = JSONObject().apply {
+                        put("departureDate", departureDate)
+                        put("returnDate", returnDate)
                     }
-                val returnDate =
-                    (destinationView.findViewById<DatePicker>(R.id.return_date_picker)).let {
-                        "${it.dayOfMonth}/${it.month + 1}/${it.year}"
+
+                    datesArray.put(datePairJson)
+                    val destinationJson = JSONObject().apply {
+                        put("dateRanges", datesArray)
+                        put("hotelReserved", hotelReserved)
+                        put("hotelAddress", hotelAddress)
+                        put("country", country)
+                        put("city", city)
+                        put("budget", budget)
+                        put("purpose", purpose)
                     }
-                val hotelReserved =
-                    destinationView.findViewById<CheckBox>(R.id.hotel_reserved_checkbox).isChecked
-                val hotelAddress =
-                    destinationView.findViewById<EditText>(R.id.hotel_address_edit_text).text.toString()
-                val country =
-                    destinationView.findViewById<EditText>(R.id.destination_country_edit_text).text.toString()
-                val city =
-                    destinationView.findViewById<EditText>(R.id.destination_city_edit_text).text.toString()
-                val budget =
-                    destinationView.findViewById<EditText>(R.id.budget_edit_text).text.toString()
-                val purpose =
-                    destinationView.findViewById<EditText>(R.id.travel_purpose_edit_text).text.toString()
 
-                val destinationJson = JSONObject().apply {
-                    put("departureDate", departureDate)
-                    put("returnDate", returnDate)
-                    put("hotelReserved", hotelReserved)
-                    put("hotelAddress", hotelAddress)
-                    put("country", country)
-                    put("city", city)
-                    put("budget", budget)
-                    put("purpose", purpose)
+                    destinations.put(destinationJson)
                 }
-                destinations.put(destinationJson)
-            }
-
-
-            val travelers = JSONArray()
-            for (i in 0 until travelersLayout.childCount) {
-                val travelerView = travelersLayout.getChildAt(i)
-
-                val travelerName =
-                    travelerView.findViewById<EditText>(R.id.additional_traveler_name_edit_text).text.toString()
-                val travelerBirthDatePicker =
-                    travelerView.findViewById<DatePicker>(R.id.additional_traveler_birth_date_picker)
-                val day = travelerBirthDatePicker.dayOfMonth
-                val month = travelerBirthDatePicker.month + 1
-                val year = travelerBirthDatePicker.year
-
-
-                val travelerBirthDate = "$day/$month/$year"
-
-                val travelerGender =
-                    travelerView.findViewById<Spinner>(R.id.additional_traveler_gender_spinner).selectedItem.toString()
-                val travelerFiliation =
-                    travelerView.findViewById<EditText>(R.id.additional_traveler_filiation_edit_text).text.toString()
-
-                val travelerJson = JSONObject().apply {
-                    put("name", travelerName)
-                    put("birthdate", travelerBirthDate)
-                    put("gender", travelerGender)
-                    put("filiation", travelerFiliation)
-                }
-                travelers.put(travelerJson)
             }
 
 
@@ -157,11 +236,11 @@ class TravelRequestActivity : AppCompatActivity() {
                     put("gender", gender)
                 })
                 put("destinations", destinations)
-                put("travelers", travelers)
+                put("travelers", JSONArray(travelersList))
             }
 
             val mediaType = "application/json; charset=utf-8".toMediaTypeOrNull()
-            val requestBody = RequestBody.create(mediaType, jsonBody.toString())
+            val requestBody = jsonBody.toString().toRequestBody(mediaType)
 
             Log.d("TravelRequestActivity", "submitForm: Corpo da requisição criado: $jsonBody")
 
@@ -226,3 +305,7 @@ class TravelRequestActivity : AppCompatActivity() {
         finish()
     }
 }
+
+
+
+
